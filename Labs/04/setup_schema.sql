ec71e08288a737685d578d649d160c82e1959622
Syed Ukkashah <115150510+syedukkashah@users.noreply.github.com>
2025-10-08 14:20:07 +0500

    Update setup_schema.sql

--- Labs/04/setup_schema.sql
+++ Labs/04/setup_schema.sql
@@ -1,57 +1,99 @@
-CREATE TABLE department (
-    id NUMBER PRIMARY KEY,
-    name VARCHAR2(30)
+create table department (
+  dept_id number primary key,
+  dept_name varchar2(50)
 );
 
-CREATE TABLE faculty (
-    id NUMBER PRIMARY KEY,
-    name VARCHAR2(30),
-    salary NUMBER,
-    joining_date DATE 
+create table student (
+  student_id number primary key,
+  student_name varchar2(50),
+  dept_id number,
+  gpa number(3,2),
+  fee_paid int,
+  foreign key (dept_id) references department(dept_id) on delete set null
 );
 
-CREATE TABLE students (
-    id NUMBER PRIMARY KEY,
-    stdName VARCHAR2(30),
-    email VARCHAR2(20),
-    password VARCHAR2(20),
-    age NUMBER,
-    city VARCHAR2(20),
-    salary NUMBER,
-    dept_id NUMBER,
-    f_id NUMBER,
-    gpa NUMBER(3,1),
-    fee NUMBER,
-    CONSTRAINT fk_dept_id FOREIGN KEY (dept_id) REFERENCES department(id),
-    CONSTRAINT fk_fac_id FOREIGN KEY (f_id) REFERENCES faculty(id)
+create table faculty (
+  faculty_id int primary key,
+  faculty_name varchar2(50),
+  dept_id number,
+  salary number,
+  joining_date date,
+  foreign key (dept_id) references department(dept_id) on delete set null
 );
 
-
-CREATE TABLE courses (
-    course_id NUMBER PRIMARY KEY,
-    course_name VARCHAR2(30),
-    faculty_id NUMBER, 
-    CONSTRAINT fk_course_faculty FOREIGN KEY (faculty_id) REFERENCES faculty(id)
+create table course (
+  course_id number primary key,
+  course_name varchar2(50),
+  dept_id number,
+  fee number,
+  foreign key (dept_id) references department(dept_id) on delete set null
 );
 
-
-CREATE TABLE enrollments (
-    enrollment_id NUMBER PRIMARY KEY,
-    student_id NUMBER,
-    course_id NUMBER,
-    CONSTRAINT fk_enroll_student FOREIGN KEY (student_id) REFERENCES students(id),
-    CONSTRAINT fk_enroll_course FOREIGN KEY (course_id) REFERENCES courses(course_id)
+create table enrollment (
+  student_id number,
+  course_id number,
+  primary key(student_id, course_id),
+  foreign key (student_id) references student(student_id) on delete cascade,
+  foreign key (course_id) references course(course_id) on delete cascade
 );
 
-
-CREATE TABLE Retired_Faculty (
-    id NUMBER PRIMARY KEY,
-    name VARCHAR2(30),
-    joining_date DATE
+create table faculty_course (
+  faculty_id number,
+  course_id number,
+  primary key(faculty_id, course_id),
+  foreign key (faculty_id) references faculty(faculty_id) on delete cascade,
+  foreign key (course_id) references course(course_id) on delete cascade
 );
 
 
-CREATE TABLE Unassigned_Faculty (
-    id NUMBER PRIMARY KEY,
-    name VARCHAR2(30)
-);
+insert into department values (1, 'CS');
+insert into department values (2, 'EE');
+insert into department values (3, 'FT');
+
+
+insert into student values (1, 'Ali', 1, 3.8, 60000);
+insert into student values (2, 'Bob', 1, 3.2, 55000);
+insert into student values (3, 'Charlie', 2, 2.8, 45000);
+insert into student values (4, 'David', 2, 3.5, 70000);
+insert into student values (5, 'Eve', 3, 2.9, 40000);
+insert into student values (6, 'Frank', 3, 3.9, 90000);
+
+
+insert into faculty values (1, 'George', 1, 150000, to_date('01-01-2000', 'DD-MM-YYYY'));
+insert into faculty values (2, 'Ali', 1, 120000, to_date('15-03-2005', 'DD-MM-YYYY'));
+insert into faculty values (3, 'Robert', 2, 90000, to_date('22-05-2010','DD-MM-YYYY'));
+insert into faculty values (4, 'Ahmed', 3, 110000, to_date('11-07-2012','DD-MM-YYYY'));
+insert into faculty values (5, 'Hank', 2, 130000, to_date('09-09-1998', 'DD-MM-YYYY'));
+
+
+insert into course values (1, 'DBMS', 1, 20000);
+insert into course values (2, 'Networks', 1, 15000);
+insert into course values (3, 'Circuits', 2, 18000);
+insert into course values (4, 'Marketing', 3, 17000);
+insert into course values (5, 'AI', 1, 25000);
+
+
+insert into enrollment values (1, 1);
+insert into enrollment values (1, 2);
+insert into enrollment values (1, 5);
+insert into enrollment values (2, 1);
+insert into enrollment values (2, 5);
+insert into enrollment values (3, 3);
+insert into enrollment values (4, 3);
+insert into enrollment values (4, 2);
+insert into enrollment values (5, 4);
+insert into enrollment values (6, 4);
+insert into enrollment values (6, 1);
+
+
+insert into faculty_course values (1,1);
+insert into faculty_course values (1,2);
+insert into faculty_course values (2,5);
+insert into faculty_course values (3,3);
+insert into faculty_course values (4,4);
+
+select * from department;
+select * from student;
+select * from faculty;
+select * from course;
+select * from enrollment;
